eedc0b9c2327b17ff77e37a59f7210a4ace8136f
BenjaminChan <benjamin.v.chan@gmail.com>
2025-03-31 22:50:23 -0700

    utils script complete

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,3 +1,13 @@
-# - Provide utility function for:
-#   - Saving list of dicts to CSV
-#   - Setting random seed for reproducibility
+import csv
+import random
+
+def set_seed(seed=42):
+    random.seed(seed)
+
+def save_dicts_to_csv(dict_list, filename):
+    if not dict_list:
+        return
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=dict_list[0].keys())
+        writer.writeheader()
+        writer.writerows(dict_list)
